38967206e19538fecbb474ad317354edb0fbe5d4
kjuulh <contact@kjuulh.io>
2023-05-27 15:43:43 +0200

    feat: test again
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
-<h1 class="text-3xl font-bold underline">Hello world! dev</h1>
+<h1 class="text-3xl font-bold underline">Hello world! dev 2</h1>
 
 <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
 	}
 </style>
-
